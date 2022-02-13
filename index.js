e38a816997a2fdc43cd87372e3b56a1b4305488a
spreyo <60149407+spreyo@users.noreply.github.com>
2022-02-13 13:50:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function lbsToKg(lbs) {
+    var kgs = lbs / 2.2046
+    return kgs;
+}
 
+var label = document.getElementById("myLabel");
+var input = document.getElementById("myText");
+var button = document.getElementById("myButton");
+
+button.onclick = function() {
+    label.textContent = Math.round(lbsToKg(Number(input.value))) + " kg";
+
+}
